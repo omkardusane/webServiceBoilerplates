06d0dc54e1df50e9d68567420945e4d7737e14b1
Omkar Dusane <omkardusane@gmail.com>
2017-04-01 20:26:19 +0530

    Boilerplates better

--- server/index.js
+++ server/index.js
@@ -4,11 +4,16 @@ var dbconn = require('./api/config/connection.js')
 
 app.use('/', express.static('public'))
 app.use('/ops', express.static('ops_site'))
-dbconn.connect(function(){
+
+let lift = function(){
   var r_api = require('./api/r_api');
   app.use('/api/',r_api);
-
   app.listen(1337,function(){
     console.log("listening on 1337");
   });
-});
+};
+
+// Use this for when you dont have MongoDb connection
+lift();
+// Uncomment and use this lift to have mongoDB Connection
+//dbconn.connect(lift);
